7f6e4563aaf9bfa4634f7866d8d579941e3593c9
haromiki <haromiki@gmail.com>
2025-08-20 19:10:14 +0000

    app 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { useLocation, Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -9,6 +9,10 @@ import Dashboard from "@/pages/dashboard";
 import NotFound from "@/pages/not-found";
 
 function Router() {
+  // 👇️ DO NOT MODIFY BELOW: Server-specific routing fix (snaver base)
+  useLocation({ base: "/snaver" });
+  // 👆️ DO NOT MODIFY ABOVE
+
   const { user, isLoading } = useAuth();
 
   if (isLoading) {
